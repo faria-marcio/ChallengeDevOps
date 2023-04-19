e0cb30a00891f4b02e635d9348e407ae735cf5dc
faria-marcio <marciodefaria@gmail.com>
2023-04-19 11:57:19 +1000

    Added terraform as IaC

---
+++ challenges/terraform/_provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "dnx-labs"
+    key = "terraform-state/terraform.tfstate"
+    region  = "ap-southeast-2"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.30.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-southeast-2"
+}
